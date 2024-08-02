f2922d5a7f4779dff76f9646424f592fc2a09c3a
Md Abdul Hai <abdulhai250620@gmail.com>
2024-08-02 19:04:37 +0600

    Hello world

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ostad/add_new_screen.dart';
+
+enum PopupMenumalmosla { edit, delete }
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,22 +23,53 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView.builder(
         itemCount: 15,
         itemBuilder: (context, index) {
-          return const ListTile(
-            title: Text("Product List"),
-            leading: CircleAvatar(),
-            subtitle: Wrap(
+          return ListTile(
+            title: const Text("Product List"),
+            leading: const CircleAvatar(),
+            subtitle: const Wrap(
               spacing: 10,
               children: [
                 Text("Product Code"),
                 Text("Quantity"),
-                Text("Unit Price", style: TextStyle(color: Colors.amber),),
-                Text("Total Price", style: TextStyle(color: Colors.red),),
+                Text(
+                  "Unit Price",
+                  style: TextStyle(color: Colors.amber),
+                ),
+                Text(
+                  "Total Price",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ],
+            ),
+            trailing: PopupMenuButton<PopupMenumalmosla>(
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                    value: PopupMenumalmosla.edit,
+                    child: Row(
+                      children: [
+                        Icon(Icons.edit),
+                        Text("Edit"),
+                      ],
+                    )),
+                const PopupMenuItem(
+                    value: PopupMenumalmosla.delete,
+                    child: Row(
+                      children: [
+                        Icon(Icons.delete),
+                        Text("Delete"),
+                      ],
+                    )),
               ],
             ),
             // trailing: ,
           );
         },
       ),
+      floatingActionButton: FloatingActionButton.extended(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const AddNewScreen()));
+      },
+          icon: const Icon(Icons.add),
+          label: const Text("Add")),
     );
   }
 }
